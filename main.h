f4c6adbfd7944ad80f15c511f9d857d3058055af
harika-konathala <123183207+harika-konathala@users.noreply.github.com>
2023-10-07 22:33:19 +0530

    Add files via upload

---
+++ main.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.h
+ * Author: HP
+ *
+ * Created on 7 October, 2023, 9:59 PM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
